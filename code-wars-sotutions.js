a2d79a9e0144fdaa403f1c1ffdd71355fb8b07c7
Nayan Prajapati <111540429+Nayanp35@users.noreply.github.com>
2023-01-03 15:27:24 -0500

    Update code-wars-sotutions.js

--- code-wars-sotutions.js
+++ code-wars-sotutions.js
@@ -1,1 +1,11 @@
 // Copy/Paste you solutions below. Each solution should be accompanied by a multiline comment explaining the solution.
+function createPhoneNumber(numbers){
+  // The phone number has to be in this format. Therefore I initialized the format variable with the format that the phone number needs to be in. 
+  let format = '(xxx) xxx-xxxx';
+  // Then using for loop, I'm going to replace every "x", with the numbers in an array. 
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i]);
+  }
+  return format;
+}
+
